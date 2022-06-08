ca3d5b2b5bf437555d75769c82aca55e37867358
unknown <lucasmgsilva@hotmail.com>
2022-06-08 14:19:42 -0300

    Renderizaçoes no react

---
+++ components/SearchResults.tsx
@@ -0,0 +1,23 @@
+import { ProductItem } from "./ProductItem";
+
+interface SearchResultsProps {
+    results: Array<{
+        id: number;
+        price: number;
+        title: string;
+    }>
+}
+
+export function SearchResults({results}: SearchResultsProps) {
+    return (
+        <div>
+            {
+                results.map((product, i) => {
+                    return (
+                        <ProductItem product={product} key={i}/>
+                    )
+                })
+            }
+        </div>
+    )
+}
