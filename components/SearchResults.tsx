ea4d6cd61af224ebaf5196832dc7148caad9c306
unknown <lucasmgsilva@hotmail.com>
2022-06-13 12:44:36 -0300

    Utilizando memo para melhorar performance

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -21,3 +21,16 @@ export function SearchResults({results}: SearchResultsProps) {
         </div>
     )
 }
+
+/**
+ * 1. Criar uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ */
+
+/**
+ * 1. Pure Function Components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size
+ */
