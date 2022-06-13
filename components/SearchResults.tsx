4ff68182f34283b2f30daa01311e9fbef7377b1c
Usuario <usuario@etecnh.sp.gov.br>
2022-06-13 20:43:22 -0300

    Virtualização

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { List, AutoSizer, ListRowRenderer } from 'react-virtualized';
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -12,10 +13,24 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({totalPrice, results, onAddToWishlist}: SearchResultsProps) {
+    const rowRenderer: ListRowRenderer = ({index, key, style}) => {
+        return (
+            <div
+                key={key}
+                style={style}
+            >
+                <ProductItem 
+                    product={results[index]}
+                    onAddToWishlist={onAddToWishlist}
+                />
+            </div>
+        )
+    }
+
     return (
         <div>
             {
-                results.map(product => {
+                /* results.map(product => {
                     return (
                         <ProductItem 
                             key={product.id}
@@ -23,7 +38,16 @@ export function SearchResults({totalPrice, results, onAddToWishlist}: SearchResu
                             onAddToWishlist={onAddToWishlist}
                         />
                     )
-                })
+                }) */
+
+                <List 
+                    height={300}
+                    rowHeight={30}
+                    width={900}
+                    overscanRowCount={5}
+                    rowCount={results.length}
+                    rowRenderer={rowRenderer}
+                />
             }
         </div>
     )
